d9b9cba9e743482e46b781098e7349c5799697de
Georgian Popovici <Georgian.Popovici@fntsoftware.com>
2020-05-19 10:58:00 +0200

    add image placeholder

--- src/app/components/create-test/create-test.component.ts
+++ src/app/components/create-test/create-test.component.ts
@@ -27,7 +27,7 @@ export class CreateTestComponent implements OnInit {
   public currentUser: UserModel = new UserModel();
   public showHomeButton = true;
 
-  public imgSrc: string;
+  public imgSrc: string = '/assets/img/image_placeholder.jpg';
   public selectedImage: any = null;
 
   public questionIsAdded = false;
@@ -77,6 +77,8 @@ export class CreateTestComponent implements OnInit {
         // reset form after question is added;
         form.resetForm();
         (document.getElementById('imageUrl') as HTMLInputElement).value = '';
+        this.imgSrc = '/assets/img/image_placeholder.jpg';
+        this.selectedImage = null;
 
         this.questionIsAdded = true;
         setTimeout(() => this.closeQuestionAlerts(), 3000);
